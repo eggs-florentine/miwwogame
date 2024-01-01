b038e03bc3cd34d6de8043b8de0927ce4595e4ce
unknown <bigcheesemiro@gmail.com>
2024-01-01 23:07:47 +0000

    setup pygame + basic demo script

--- Main.py
+++ Main.py
@@ -1,1 +1,26 @@
-print("testing 123")
+import pygame
+pygame.init()
+
+
+screen = pygame.display.set_mode([500, 500])
+
+
+running = True
+while running:
+
+    # Did the user click the window close button?
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Fill the background with whited
+    screen.fill((255, 255, 255))
+
+    # Draw a solid blue circle in the center
+    pygame.draw.circle(screen, (0, 0, 255), (250, 250), 75)
+
+    # Flip the display
+    pygame.display.flip()
+
+# Done! Time to quit.
+pygame.quit()
